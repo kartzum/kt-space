b58833e90cac5431ec8ae5e9120b93f472944036
kartzum <kartzum@gmail.com>
2021-07-22 10:49:57 +0300

    sf.

---
+++ sf/src/main/kotlin/aaa/abcd/sf/SfApplication.kt
@@ -0,0 +1,11 @@
+package aaa.abcd.sf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SfApplication
+
+fun main(args: Array<String>) {
+	runApplication<SfApplication>(*args)
+}
---
+++ sf/src/test/kotlin/aaa/abcd/sf/SfApplicationTests.kt
@@ -0,0 +1,13 @@
+package aaa.abcd.sf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SfApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
