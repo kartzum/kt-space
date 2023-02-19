de7020e22b3157fd8c56ee358c0b0509e058c918
kartzum <kartzum@gmail.com>
2023-02-19 19:15:45 +0300

    cm.

---
+++ cm/src/jvmMain/kotlin/sr/App.kt
@@ -0,0 +1,108 @@
+package sr
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.input.key.Key
+import todo.onKeyUp
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        RootContent(
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun RootContent(
+    modifier: Modifier = Modifier,
+) {
+    val rootStore = remember { RootStore() }
+    val state = rootStore.state
+
+    MainContent(
+        modifier = modifier,
+        inputText = state.searchText,
+        onSearchTextChanged = rootStore::onSearchTextChanged,
+        onSearchTextClicked = rootStore::onSearchTextClicked,
+    )
+}
+
+@Composable
+internal fun MainContent(
+    modifier: Modifier = Modifier,
+    inputText: String,
+    onSearchTextChanged: (String) -> Unit,
+    onSearchTextClicked: () -> Unit,
+) {
+    Column(modifier) {
+        SearchInput(
+            text = inputText,
+            onTextChanged = onSearchTextChanged,
+            onTextClicked = onSearchTextClicked,
+        )
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun SearchInput(
+    text: String,
+    onTextChanged: (String) -> Unit,
+    onTextClicked: () -> Unit,
+) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = text,
+            modifier = Modifier
+                .weight(weight = 1F)
+                .onKeyUp(key = Key.Enter, action = onTextClicked),
+            onValueChange = onTextChanged,
+            label = { Text(text = "") }
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+    }
+}
+
+data class RootState(
+    val searchText: String = "",
+)
+
+class RootStore {
+    fun onSearchTextChanged(text: String) {
+        setState { copy(searchText = text) }
+    }
+
+    fun onSearchTextClicked() {
+        setState { copy(searchText = "") }
+    }
+
+    var state: RootState by mutableStateOf(initialState())
+        private set
+
+    private fun initialState(): RootState = RootState()
+
+    private inline fun setState(update: RootState.() -> RootState) {
+        state = state.update()
+    }
+}
